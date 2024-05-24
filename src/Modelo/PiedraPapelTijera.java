c90deb147c3cd7d34218f2da33a868208efc2ce8
Andres111203 <andres.canas@correounivalle.edu.co>
2024-05-23 22:17:50 -0500

    se implementa la clase modelo del videojuego, alli se incluye la opcion del usuario y el de la maquina, ademas de un metodo para evaluar la opciones y dar retornar un valor de lo que ocurre, puede ser modificado segun sea necesario al implementar la clase vista y controlador

--- src/Modelo/PiedraPapelTijera.java
+++ src/Modelo/PiedraPapelTijera.java
@@ -1,5 +1,46 @@
 package Modelo;
 
+import java.util.Random;
+
 public class PiedraPapelTijera {
+
+    private Opciones opcUser;
+    private Opciones opcMachine;
+
+
+    public Opciones getOpcUser() {
+        return opcUser;
+    }
+    public void setOpcUser(Opciones opcUser) {
+        this.opcUser = opcUser;
+    }
+    public Opciones getOpcMachine() {
+        return opcMachine;
+    }
+
+    public void setOpcMachine() {
+        Opciones[] opc = Opciones.values();
+        int opcAleatoria = new java.util.Random().nextInt(opc.length);
+        this.opcMachine = opc[opcAleatoria];
+    }
+
+    public String jugar(Opciones opcUser, Opciones opcMachine){
+        if (opcUser == opcMachine) {
+            return "Empate";
+        }
+
+        switch (opcUser) {
+
+            case PIEDRA:
+                return (opcMachine == Opciones.TIJERA) ? "Jugador gana" : "Computadora gana";
+            case PAPEL:
+                return (opcMachine == Opciones.PIEDRA) ? "Jugador gana" : "Computadora gana";
+            case TIJERA:
+                return (opcMachine == Opciones.PAPEL) ? "Jugador gana" : "Computadora gana";
+        }
+
+        return "Error";
+    
+    }
     
 }
