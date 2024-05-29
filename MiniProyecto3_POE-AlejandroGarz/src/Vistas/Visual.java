6e631fba6601c24ea0337ee2050a14ff151d4cd6
Juan_F <juan.e.franco@correounivalle.edu.co>
2024-05-28 20:26:21 -0500

    Parte visual completada

--- MiniProyecto3_POE-AlejandroGarz/src/Vistas/Visual.java
+++ MiniProyecto3_POE-AlejandroGarz/src/Vistas/Visual.java
@@ -17,4 +17,39 @@ public class Visual extends JFrame {
     private JButton btnTijera;
     private JButton btnJugar;
     private JLabel lblResultado;
+    public Visual() {
+        juego = new PiedraPapelTijera();
+        ctrl = new controlador(juego);
+
+        setTitle("Piedra, Papel o Tijera");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        btnPiedra = new JButton("PIEDRA");
+        btnPapel = new JButton("PAPEL");
+        btnTijera = new JButton("TIJERA");
+        btnJugar = new JButton("JUGAR");
+        lblResultado = new JLabel("");
+
+        btnPiedra.addActionListener(ctrl);
+        btnPapel.addActionListener(ctrl);
+        btnTijera.addActionListener(ctrl);
+        btnJugar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                juego.setOpcMachine();
+                Opciones opcUser = juego.getOpcUser();
+                Opciones opcMachine = juego.getOpcMachine();
+                String resultado = juego.jugar(opcUser, opcMachine);
+                lblResultado.setText("Jugador: " + opcUser + ", Máquina: " + opcMachine + ". Resultado: " + resultado);
+            }
+        });
+
+        add(btnPiedra);
+        add(btnPapel);
+        add(btnTijera);
+        add(btnJugar);
+        add(lblResultado);
+    }
 }
