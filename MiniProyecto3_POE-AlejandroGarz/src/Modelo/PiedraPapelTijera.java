5203c47d68f26e81e1cee7b35cbcac7d50b085ae
Juan_F <juan.e.franco@correounivalle.edu.co>
2024-05-28 19:50:27 -0500

    Modifico el modelo

--- MiniProyecto3_POE-AlejandroGarz/src/Modelo/PiedraPapelTijera.java
+++ MiniProyecto3_POE-AlejandroGarz/src/Modelo/PiedraPapelTijera.java
@@ -3,44 +3,41 @@ package Modelo;
 import java.util.Random;
 
 public class PiedraPapelTijera {
-
     private Opciones opcUser;
     private Opciones opcMachine;
-
 
     public Opciones getOpcUser() {
         return opcUser;
     }
+
     public void setOpcUser(Opciones opcUser) {
         this.opcUser = opcUser;
     }
+
     public Opciones getOpcMachine() {
         return opcMachine;
     }
 
     public void setOpcMachine() {
         Opciones[] opc = Opciones.values();
-        int opcAleatoria = new java.util.Random().nextInt(opc.length);
+        int opcAleatoria = new Random().nextInt(opc.length);
         this.opcMachine = opc[opcAleatoria];
     }
 
-    public String jugar(Opciones opcUser, Opciones opcMachine){
+    public String jugar(Opciones opcUser, Opciones opcMachine) {
         if (opcUser == opcMachine) {
             return "Empate";
         }
 
         switch (opcUser) {
-
             case PIEDRA:
                 return (opcMachine == Opciones.TIJERA) ? "Jugador gana" : "Computadora gana";
             case PAPEL:
                 return (opcMachine == Opciones.PIEDRA) ? "Jugador gana" : "Computadora gana";
             case TIJERA:
                 return (opcMachine == Opciones.PAPEL) ? "Jugador gana" : "Computadora gana";
+            default:
+                return "Error";
         }
-
-        return "Error";
-    
     }
-    
 }
